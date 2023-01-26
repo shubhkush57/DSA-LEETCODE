398b62cbe5f799879be8e69e1cc8949297f855bc
Shubham Kushwaha <76884219+Shubham-20205157@users.noreply.github.com>
2023-01-27 00:58:27 +0530

    Time: 520 ms (60.88%), Space: 112.9 MB (87.01%) - LeetHub

--- 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.cpp
+++ 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.cpp
@@ -2,18 +2,15 @@ class Solution {
 public:
     long long maximumImportance(int n, vector<vector<int>>& roads) {
         long long sum = 0;
-        map<int,int>mp;
+        // map<int,int>mp;
+        vector<int>v(n,0);
         int n_siz = roads.size();
         for(int i = 0;i<n_siz;i++){
-            mp[roads[i][0]]++;
-            mp[roads[i][1]]++;
+            v[roads[i][0]]++;
+            v[roads[i][1]]++;
         }
         // we want to calcualte total connection of each node
-        vector<int>v;
-        for(auto it: mp){
-            v.push_back(it.second);
-            cout<<it.second<<endl;
-        }
+                
         sort(v.begin(),v.end());
         reverse(v.begin(),v.end());
         for(int i = 0;i<v.size();i++){
