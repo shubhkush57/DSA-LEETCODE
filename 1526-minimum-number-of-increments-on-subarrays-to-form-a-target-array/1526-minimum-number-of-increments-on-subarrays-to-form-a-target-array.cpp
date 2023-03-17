6cd5fb5c9e60019ffb56fc023a0e97aa7a78aff2
Shubham Kushwaha <76884219+Shubham-20205157@users.noreply.github.com>
2023-03-17 18:19:12 +0530

    Time: 132 ms (77.88%), Space: 73.1 MB (94.82%) - LeetHub

--- 1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
+++ 1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
@@ -2,54 +2,52 @@ class Solution {
 public:
    
     int minNumberOperations(vector<int>& target) {
+//         int n = target.size();
+//         // to rememove the equal things.......
+//         vector<int>tar;
+//         for(int i = 0;i<n;i++){
+//             if( i == 0){
+//                 tar.push_back(target[i]);
+//             }
+//             else if(target[i] != tar.back()){
+//                 tar.push_back(target[i]);
+//             }
+//         }
+//         // you have to just calcaulate the local maximam and then add it to the anser
+//         // ans += (local  maxima )-(previous local minima);
+//         n  = tar.size();
+//         if(n == 1){
+//             return target[0];
+//         }
+//         int prev_min = 0;
+//         int ans = 0;
+//         for(int i = 0;i<tar.size();i++){
+//             if(i == 0){
+//                 if(tar[i]>tar[i+1]){
+//                     ans += tar[i]-prev_min;
+//                 }
+//             }
+//             else if(i == n-1){
+//                 if(tar[i] > tar[i-1]){
+//                     ans += tar[i]-prev_min;
+//                 }
+//             }
+//             else{
+//                 if(tar[i]>tar[i-1] && tar[i]> tar[i+1]){
+//                     ans += tar[i]-prev_min;
+//                 }
+//                 else if(tar[i] < tar[i-1] && tar[i] < tar[i+1]){
+//                     prev_min = tar[i];
+//                 }
+               
+//             }
+//         }
+        // second way of calculating the differnce between local maxima and local minia
+        // instead of goind for direct diff we will go asceding diffence. 
         int n = target.size();
-        
-        vector<int>tar;
-        for(int i = 0;i<n;i++){
-            if( i == 0){
-                tar.push_back(target[i]);
-            }
-            else if(target[i] != tar.back()){
-                tar.push_back(target[i]);
-            }
-        }
-        n  = tar.size();
-        if(n == 1){
-            return target[0];
-        }
-        int prev_min = 0;
-        int ans = 0;
-        for(int i = 0;i<tar.size();i++){
-            if(i == 0){
-                if(tar[i]>tar[i+1]){
-                    ans += tar[i]-prev_min;
-                }
-            }
-            else if(i == n-1){
-                if(tar[i] > tar[i-1]){
-                    ans += tar[i]-prev_min;
-                }
-            }
-            else{
-                if(tar[i]>tar[i-1] && tar[i]> tar[i+1]){
-                    ans += tar[i]-prev_min;
-                }
-                else if(tar[i] < tar[i-1] && tar[i] < tar[i+1]){
-                    prev_min = tar[i];
-                }
-                // else if(target[i] > target[i-1] && target[i] >= target[i+1]){
-                //     ans += target[i]-prev_min;
-                // }
-                // else if(target[i] >= target[i-1] && target[i] > target[i+1]){
-                //     ans += target[i]-prev_min;
-                // }
-                // else if(target[i] <= target[i-1] && target[i] < target[i+1]){
-                //     prev_min = target[i];
-                // }
-                // else if(target[i] < target[i-1] && target[i] <= target[i+1]){
-                //     prev_min = target[i];
-                // }
-            }
+        int ans = target[0];
+        for(int i = 1;i<n;i++){
+            ans += max(0,target[i]-target[i-1]);
         }
         return ans;
     }
