a12b67d12da3511d8b5725fdb9e31a89d092b20b
Shubham Kushwaha <76884219+shubhkush57@users.noreply.github.com>
2024-11-04 20:14:57 +0530

    Time: 69 ms (100%), Space: 38.4 MB (100%) - LeetHub

--- 3627-find-minimum-time-to-reach-last-room-i/3627-find-minimum-time-to-reach-last-room-i.cpp
+++ 3627-find-minimum-time-to-reach-last-room-i/3627-find-minimum-time-to-reach-last-room-i.cpp
@@ -9,12 +9,8 @@ public:
         vector<vector<int>>vis(n,vector<int>(m,INT_MAX));
         int ans = INT_MAX;
         queue<pp>q;
-        // q.push({{0,1},moveTime[0][1]+1});
-        // q.push({{1,0},moveTime[1][0]+1});
         q.push({{0,0},0});
         vis[0][0] = 0;
-        // vis[0][1] = moveTime[0][1]+1;
-        // vis[1][0] = moveTime[1][0]+1;
         while(!q.empty()){
             int t = q.size();
             while(t--){
@@ -30,17 +26,13 @@ public:
                     if(nx>=0 && ny>=0 && nx<n && ny<m){
                         int cal = moveTime[nx][ny] - moves;
                         if(cal<0){
-                           // we can move freely
                             if(vis[nx][ny]>moves+1){
                                 vis[nx][ny] = moves+1;
                                 q.push({{nx,ny},vis[nx][ny]});
                             }
                         }
                         else{
-                            // if(cal%2 == 0){
-                            //     cal++;
-                                
-                            // }
+                           
                             if(vis[nx][ny]>moves+cal){
                                 vis[nx][ny] = moves+cal+1;
                                 q.push({{nx,ny},vis[nx][ny]});
@@ -50,12 +42,7 @@ public:
                 }
             }
         }
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<m;j++){
-                cout<<vis[i][j]<<' ';
-            }
-            cout<<endl;
-        }
+       
         return ans;
         
     }
