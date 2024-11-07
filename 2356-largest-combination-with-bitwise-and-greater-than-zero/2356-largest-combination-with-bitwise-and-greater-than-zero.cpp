ee60b7a3c7ab9b9f8d2435653119c3b673e873ca
Shubham Kushwaha <76884219+shubhkush57@users.noreply.github.com>
2024-11-08 00:20:34 +0530

    Time: 43 ms (19.4%), Space: 60.1 MB (74.4%) - LeetHub

--- 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.cpp
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int largestCombination(vector<int>& candidates) {
-        int siz = candidates.size();
         int largest_combination_size = 0;
         vector<int> bitInfo(32,0);
-        for(auto ele: candidates){
-            for(int i = 0;i<31;i++){
+        for(int i = 0;i<31;i++){
+            int count = 0;
+            for(auto ele: candidates){
                 if((ele & (1<<i))>0){
-                    bitInfo[i]++;
-                    largest_combination_size = max(largest_combination_size,bitInfo[i]);
+                    count++;
+                    largest_combination_size = max(largest_combination_size,count);
                 }
             }
         }
-
         return largest_combination_size;
     }
 };
