7e3e18a46871729ec87ce3749f246166d7e41d1a
Shubham Kushwaha <76884219+shubhkush57@users.noreply.github.com>
2024-11-11 02:08:40 +0530

    Time: 0 ms (100%), Space: 51.1 MB (46.39%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -2,12 +2,11 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int n = nums.size();
-        // we will take the maximum jump from the past.
-        int reach = 0;
-        for(int i = 0;i<n;i++){
-            reach = max(reach,i+nums[i]);
-            if(reach == n-1)return true;
-            if(reach == i)return false;
+        int gas = 0;
+        for(auto it: nums){
+            if(gas<0)return false;
+            else if(it>gas)gas = it;
+            gas -=1;
         }
         return true;
     }
