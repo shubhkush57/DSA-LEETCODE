2d63a1868a6dff58c66852edf88cc16425934403
Shubham Kushwaha <76884219+shubhkush57@users.noreply.github.com>
2024-11-12 22:26:41 +0530

    Time: 11 ms (50%), Space: 16.9 MB (31.36%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,3 +1,61 @@
+class TrieNode{
+public:
+    char ch;
+    vector<TrieNode*>children;
+    bool isWord;
+    TrieNode(char c){
+        this->ch = c;
+        for(int i = 0;i<26;i++){
+            this->children.push_back(NULL);
+        }
+        this->isWord = false;
+    }
+};
+class Trie{
+public:
+    TrieNode* root;
+    Trie(){
+        root = new TrieNode('/');
+    }
+    void insert(string &word,TrieNode* root,int i){
+        int n = word.size();
+        if(i>=n){
+            root->isWord = true;
+            return;
+        }
+
+        TrieNode* child;
+        char ch = word[i];
+        if(root->children[ch-'a'] != NULL){
+            child = root->children[ch-'a'];
+        }
+        else{
+            child = new TrieNode(ch);
+            root->children[ch-'a'] = child;
+        }
+        insert(word,child,i+1);
+        return;
+    }
+    void insertWord(string &word){
+        int n = word.size();
+        TrieNode* r = root;
+        insert(word,r,0);
+        return;
+    }
+    // search for the word..
+    bool search(string &word,TrieNode* root,int i){
+        int n = word.size();
+        if(i>=n)return true;
+        //now we have to consider it as the 
+        char ch = word[i];
+        if(root->children[ch-'a'] == NULL)return false;
+        return search(word,root->children[ch-'a'],i+1);
+    }
+    bool searchWord(string &word){
+        TrieNode* r = root;
+        return search(word,r,0);
+    }
+};
 class Solution {
 public:
     bool solve(string &s, int i,unordered_map<string,int>&mp,vector<int>&dp){
@@ -19,18 +77,20 @@ public:
         int n = s.size();
         vector<int>dp(n+1,false);
         dp[n] = true;
-        unordered_map<string,int>mp; 
+        unordered_map<string,int>mp;
+        Trie* node = new Trie(); 
         for(auto it: wordDict){
-            mp[it]++;
+            // mp[it]++;
+            node->insertWord(it);
         }
         for(int i = n-1;i>=0;i--){
             bool ans = false;
-            string re = "";
+            TrieNode* root = node->root;
             for(int j = i;j<n;j++){
-                re.push_back(s[j]);
-                if(mp.count(re)){
-                    ans = ans or dp[j+1];
-                }
+                if(root->children[s[j]-'a'] == NULL)break;
+                if(root->children[s[j]-'a']->isWord)
+                ans = ans or dp[j+1];
+                root = root->children[s[j]-'a'];
             }
             dp[i] = ans;
         }
