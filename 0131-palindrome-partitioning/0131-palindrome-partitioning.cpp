40a86b284450b821be802b0cbb0733ca3d6fccf6
Shubham Kushwaha <76884219+shubhkush57@users.noreply.github.com>
2024-11-17 15:48:01 +0530

    Time: 33 ms (96.89%), Space: 53.1 MB (81.72%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,37 +1,35 @@
 class Solution {
 public:
-    bool isPalindrome(string s,int st,int e){
-        while(st<=e){
-            if(s[st] != s[e]){
-                return false;
-            }
-            st++;
-            e--;
+    bool isPalind(string &s,int start,int end){
+        int n = s.size();
+        while(start<end){
+            if(s[start] != s[end])return false;
+            start++;end--;
         }
         return true;
-            
     }
-    void solve(vector<vector<string>>&ans,vector<string>&pp,int i, string s){
-        if(i == s.size()){
-            ans.push_back(pp);
+    void backtrack(string &s, vector<vector<string>>&ans,vector<string>&re,int start){
+        int n = s.size();
+        if(start>=n){
+            if(re.size()>0){
+                ans.push_back(re);
+            }
             return;
         }
-        for(int k = i;k<s.size();k++){
-            // it is dfs
-            if(isPalindrome(s,i,k)){
-                pp.push_back(s.substr(i,k-i+1));
-                solve(ans,pp,k+1,s);
-                pp.pop_back();
+
+        for(int i = start;i<n;i++){
+            if(isPalind(s,start,i)){
+                re.push_back(s.substr(start,i-start+1));
+                backtrack(s,ans,re,i+1);
+                re.pop_back();
             }
         }
         return;
     }
     vector<vector<string>> partition(string s) {
         vector<vector<string>>ans;
-        // now take the values of the string
-        int i = 0;
-        vector<string>pp;
-        solve(ans,pp,i,s);
+        vector<string> re;
+        backtrack(s,ans,re,0);
         return ans;
     }
 };
