3c219026be32fab17a4125e600dc7cbc7681d7d6
Shubham Kushwaha <76884219+shubhkush57@users.noreply.github.com>
2024-11-17 21:09:33 +0530

    Time: 0 ms (100%), Space: 19.3 MB (48.03%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,30 +1,19 @@
 class Solution {
 public:
-    int solve(vector<int>&nums,int i,vector<int>&dp){
-        int n  = nums.size(); // now take the value of the 
-        if(i == n-1){
-            return 0;
-        }
-        // value 
-        // minimum number of ways
-        // now take the valuef
-        if(dp[i] != -1){
-            return dp[i];
-        }
-        int ans = 10000;
-        for(int j = i+1;j<=min(n-1,i+nums[i]);j++){
-            // now jump to the index and 
-            if(nums[j] != 0|| (nums[j] == 0 && j == n-1))
-            ans = min(ans,1+solve(nums,j,dp));
-        }
-        return dp[i] = ans;
-    }
     int jump(vector<int>& nums) {
-        int start = 0;
-        // mimiu numbers of jump
-        // each index pe maximum no of jumps requrired store
-        // max heap se to nahi hoga
-        vector<int>dp(nums.size()+1,-1);
-        return solve(nums,0,dp);
+        // nearest points farthest point and jumps...
+        int n = nums.size();
+        int near = 0,far = 0,jumps = 0;
+        while(far<n-1){
+            int farthest = 0;
+            for(int i = near;i<=far;i++){
+                farthest = max(farthest,i+nums[i]);
+            }
+            near = far+1;
+            far = farthest;
+            jumps++;
+        }
+
+        return jumps;
     }
 };
