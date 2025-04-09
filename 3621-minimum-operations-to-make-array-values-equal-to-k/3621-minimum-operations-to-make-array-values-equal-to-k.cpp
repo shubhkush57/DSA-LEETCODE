e0a8e465ebc6ff6e609f108c9b0835b96f1490f8
Shubham Kushwaha <76884219+shubhkush57@users.noreply.github.com>
2025-04-09 09:43:02 +0530

    Time: 3 ms (99.06%), Space: 32.6 MB (24.53%) - LeetHub

--- 3621-minimum-operations-to-make-array-values-equal-to-k/3621-minimum-operations-to-make-array-values-equal-to-k.cpp
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/3621-minimum-operations-to-make-array-values-equal-to-k.cpp
@@ -4,15 +4,19 @@ public:
         int n = nums.size();
         set<int>st;
         int i = n-1;
-        
-        sort(nums.begin(),nums.end());
-        if(k>nums[0])return -1;
-        while(i>=0){
-            if(nums[i]>k){
-                st.insert(nums[i]);
-            }
-            i--;
+        for(auto it: nums){
+            if(it<k)return -1;
+            else if(it>k)st.insert(it);
         }
+        
+        // sort(nums.begin(),nums.end());
+        // if(k>nums[0])return -1;
+        // while(i>=0){
+        //     if(nums[i]>k){
+        //         st.insert(nums[i]);
+        //     }
+        //     i--;
+        // }
         return st.size();
     }
 };
