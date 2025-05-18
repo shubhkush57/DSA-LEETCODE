3a2b7024ebdd9af66af89265cf14b571cbe973a1
Shubham Kushwaha <76884219+shubhkush57@users.noreply.github.com>
2025-05-18 21:13:53 +0530

    Time: 0 ms (100%), Space: 15.7 MB (67.68%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,35 +1,24 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        // next lexicographical order arrangement of 
-        // nums[i]<nums[i+1]
-        // from the back find the number which is greater than nums say index j element
-        // now swap nums[i],nums[j]
-        // and sort the elements from begin()+i,end();
-        int n= nums.size();
+        int n = nums.size();
         int ind1 = -1;
-        for(int i = n-1;i>=0;i--){
-            if(i != n-1){
-                if(nums[i]<nums[i+1]){
-                    ind1 = i;
-                    break;
-                }
-            }
-        }
-        if(ind1 == -1){
-            reverse(nums.begin(),nums.end());
-            return;
-        }
-        int ind2= -1;
-        for(int i = n-1;i> ind1;i--){
-            if(nums[i] > nums[ind1]){
-                ind2 = i;
+        for(int i = n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                ind1 = i;
                 break;
             }
         }
-        cout<<ind1<<' '<<ind2<<endl;
-        swap(nums[ind1],nums[ind2]);
-        // cout<<nums[0]<<' '<<nums[1]<<' '<<nums[2]<<endl;
+        if(ind1 == -1){
+            reverse(nums.begin(),nums.end());return;
+        }
+        // we have found some index... we have to swap with
+        for(int i = n-1;i>ind1;i--){
+            if(nums[i]>nums[ind1]){
+                swap(nums[i],nums[ind1]);
+                break;
+            }
+        }
         sort(nums.begin()+ind1+1,nums.end());
         return;
     }
