82da100fb6970dc8cf9323d47cc1c6d0bbc47dc4
Shubham Kushwaha <76884219+shubhkush57@users.noreply.github.com>
2025-05-28 08:42:24 +0530

    Time: 0 ms (100%), Space: 8.4 MB (69.39%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.cpp
+++ 0778-reorganize-string/0778-reorganize-string.cpp
@@ -1,36 +1,38 @@
 class Solution {
 public:
-    #define int2 long long
-    #define pp pair<int,char>
     string reorganizeString(string s) {
-       unordered_map<char,int2>mp;
-       for(auto it: s)mp[it]++;
-       priority_queue<pp>pq;
-       for(auto it: mp){
-        pq.push({it.second,it.first});
-       }
-       string ans = "";
-        while(pq.size()>1){
-            auto firstChar = pq.top();pq.pop();
-            auto secondChar = pq.top();pq.pop();
-            ans.push_back(firstChar.second),ans.push_back(secondChar.second);
-            if(firstChar.first-1>0){
-                pq.push({firstChar.first-1,firstChar.second});
-            }
-            if(secondChar.first-1>0){
-                pq.push({secondChar.first-1,secondChar.second});
+        // check if maxFreq ele count is less than size+1/2 , otherwise NP; and place at alernative place ... and if they are exceeding size , start from 1;
+        unordered_map<char,int>mp;
+        int n = s.size();
+        int maxCount = 0,maxEle = '.';
+        for(auto it: s){
+            mp[it]++;
+            if(mp[it]>maxCount){
+                maxCount = mp[it];
+                maxEle = it;
             }
         }
-        if(pq.size() ==1){
-            auto ele = pq.top();pq.pop();
-            if(ele.first>1)return "";
-            if(ans.size()>0 &&ele.second == ans.back())return "";
-            ans.push_back(ele.second);
+        // now we have to find
+        if(maxCount>(n+1)/2) return "";
+        int j = 0;
+        while(mp[maxEle]>0){
+            s[j] = maxEle;
+            j+=2;
+            mp[maxEle]--;
         }
+        
+        for(auto it: mp){
+           
+            while(mp[it.first]>0){
+                if(j>=n){
+                    j = 1;
+                }
+                 s[j] = it.first;
+                 mp[it.first]--;
+                 j+=2;   
+            }
+        }
+        return s;
 
-
-
-
-       return ans;
     }
 };
