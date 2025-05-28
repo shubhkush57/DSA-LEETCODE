ec174679863c82162d26a679fcef7da5819147df
Shubham Kushwaha <76884219+shubhkush57@users.noreply.github.com>
2025-05-28 11:51:37 +0530

    Time: 11 ms (24.36%), Space: 20.3 MB (22.47%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,37 +1,34 @@
 class Solution {
 public:
-    bool dfs(vector<int>&indegree,vector<bool>&vis,vector<vector<int>>&adj,int node){
-        for(auto it: adj[node]){
-            if(vis[it])return false;
+    bool dfs(vector<vector<int>>&adjList,vector<int>&indegree,vector<bool>&vis,int node){
+        for(auto it: adjList[node]){
+            if(vis[it]) return false;
             indegree[it]--;
             if(indegree[it] == 0){
                 vis[it] = true;
-
-                if(!dfs(indegree,vis,adj,it))return false;
+                if(!dfs(adjList,indegree,vis,it))return false;
             }
-            
         }
         return true;
     }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        // indegree and dfs
         vector<int>indegree(numCourses,0);
-        vector<vector<int>>adj(numCourses,vector<int>());
+        vector<vector<int>>adjList(numCourses,vector<int>());
         for(auto it: prerequisites){
-            adj[it[1]].push_back(it[0]);
+            adjList[it[1]].push_back(it[0]);
             indegree[it[0]]++;
         }
         vector<bool>vis(numCourses,false);
         for(int i = 0;i<numCourses;i++){
             if(indegree[i] == 0 && vis[i] == false){
                 vis[i] = true;
-                if(!dfs(indegree,vis,adj,i))return false;
+                if(!dfs(adjList,indegree,vis,i))return false;
             }
         }
-        
         for(auto it: vis){
-            if(it == false)return false;
+            if(!it)return false;
         }
         return true;
-
     }
 };
