c2b1b4a51effc53271af3349a81bbda4c7ef32c4
Shubham Kushwaha <76884219+shubhkush57@users.noreply.github.com>
2025-05-29 15:48:55 +0530

    Time: 0 ms (100%), Space: 27.8 MB (76.53%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -11,7 +11,7 @@
  */
 class Solution {
 public:
-    int solve(TreeNode* root,int &ans){
+    int solve(TreeNode* root, int &ans){
         if(root == NULL)return 0;
         int left = max(solve(root->left,ans),0);
         int right = max(solve(root->right,ans),0);
@@ -19,20 +19,8 @@ public:
         return root->val+max(left,right);
     }
     int maxPathSum(TreeNode* root) {
-        // so this is our solution...
-        // so moving on the coding part..
         int ans = INT_MIN;
         solve(root,ans);
         return ans;
     }
 };
-
-// Algo...
-// 1. base case.. if(root == NULL)return 0;
-
-// 2. calcuate left = max(solve(root->left),0);
-// 3. calculate right = max(solve(root->right),0);
-
-// 4. ans= max(ans,root->val+left+right);
-
-// return root->val+max(left,right);
