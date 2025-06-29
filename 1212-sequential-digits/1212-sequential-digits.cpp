49c414128024d3ef71e54cff572d892746475a70
Shubham Kushwaha <76884219+shubhkush57@users.noreply.github.com>
2025-06-29 23:57:40 +0530

    Time: 0 ms (100%), Space: 8.1 MB (96.01%) - LeetHub

--- 1212-sequential-digits/1212-sequential-digits.cpp
+++ 1212-sequential-digits/1212-sequential-digits.cpp
@@ -2,13 +2,11 @@ class Solution {
 public:
     vector<int> sequentialDigits(int low, int high) {
         vector<int>ans;
-
         for(int i = 1;i<=8;i++){
-            int val = i;
-            int count = i+1;
+            int val = i,count = i+1;
             while(val<=high && count<=9){
                 val = val*10+count;
-                if(val<=high && val >=low){
+                if(val>= low && val<=high){
                     ans.push_back(val);
                 }
                 count++;
