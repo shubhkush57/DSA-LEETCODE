636f3ae4e4286f4740115e44d207046e0764b08c
Shubham Kushwaha <76884219+shubhkush57@users.noreply.github.com>
2025-07-15 23:48:15 +0530

    Time: 38 ms (96.35%), Space: 29 MB (90.2%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
         int n = nums.size();
-        vector<vector<int>>ans; // answer array
-        sort(nums.begin(),nums.end()); // sorting the array
-        // they should not contiain duplicates..
-        for(int i = 0;i<n;i++){
-            if(i>0 && nums[i] == nums[i-1])continue;
-            int target = -nums[i];
-            int start  = i+1;
-            int end = n-1;
-            // we will be seraching the each element..
+        vector<vector<int>>ans;
+        for(int i = 0;i<n-2;i++){
+            int target = -nums[i]; // this is going to be mine target...
+            if(i!= 0 && nums[i]== nums[i-1])continue;
+            int start = i+1, end = n-1;
             while(start<end){
-                int currSum = nums[start]+nums[end];
-                if(currSum == target){
-                    // we have out answer...
+                int currentSum = nums[start]+nums[end];
+                if(currentSum == target){
                     ans.push_back({nums[i],nums[start],nums[end]});
                     start++;
-                    while(start<n && nums[start] == nums[start-1]){
-                        start++;
-                    }
+                    while(start<n && nums[start] == nums[start-1])start++;
                     end--;
-                    while(end>=0 && nums[end] == nums[end+1]){
-                        end--;
-                    }
+                    while(end>=0 && nums[end] == nums[end+1])end--;
+                    
                 }
-                else if(currSum>target){
+                else if(currentSum>target){
                     end--;
                 }
                 else{
@@ -33,6 +26,7 @@ public:
                 }
             }
         }
+
         return ans;
     }
 };
