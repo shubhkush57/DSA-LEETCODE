484fbf30a9c496576d672860c5893a333f45d6dd
Shubham Kushwaha <76884219+shubhkush57@users.noreply.github.com>
2025-07-22 00:24:10 +0530

    Time: 0 ms (100%), Space: 54.9 MB (74.66%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,39 +1,24 @@
 class Solution {
 public:
+    #define int2 long long
     int firstMissingPositive(vector<int>& nums) {
-        
-
-        // swap , take the next will keep it swapping in ti's correct postion
-        // if resulting of the array size or it'is in correct
         int n = nums.size();
         for(int i = 0;i<n;i++){
-            // each element
-            // element if it's greater thatns ize..
-            if(nums[i]> n || nums[i]<=0){
-                continue;
-            }
-            else{
-                int ele = nums[i];
-                int des_indx = ele-1;
-                int curr = i;
-                while(ele < n && ele >0 && nums[curr] != nums[des_indx]){
-                    // swap the number
-                    int ele_pres = nums[des_indx];
-                    nums[des_indx] = ele;
-                    nums[curr] = ele_pres;
-                    ele = nums[curr];
-                    curr = i;
-                    des_indx = nums[curr]-1;
-                }
+            if(nums[i]<=0 || nums[i]>n)continue;
+            int ele = nums[i], dest_indx = ele-1,curr = i;
+            while(ele>0 && ele<n && nums[curr] != nums[dest_indx]){
+                int temp = nums[dest_indx];
+                nums[dest_indx] = ele;
+                nums[curr] = temp;
+                curr = i;
+                ele = nums[curr];
+                dest_indx = ele-1;
+
             }
         }
-        int ans = 1;
         for(int i = 0;i<n;i++){
-            if(nums[i] != ans){
-                return ans;
-            }
-            ans++;
+            if(nums[i] != i+1)return i+1;
         }
-        return ans;
+        return n+1;
     }
 };
