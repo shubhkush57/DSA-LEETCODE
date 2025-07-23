68aefb43601da5198f7ada0948c566ff1aa02f0e
Shubham Kushwaha <76884219+shubhkush57@users.noreply.github.com>
2025-07-23 22:29:35 +0530

    Time: 101 ms (7.36%), Space: 137.1 MB (37.03%) - LeetHub

--- 1023-time-based-key-value-store/1023-time-based-key-value-store.cpp
+++ 1023-time-based-key-value-store/1023-time-based-key-value-store.cpp
@@ -1,32 +1,29 @@
 class TimeMap {
 public:
-    #define pp pair<int,string>
-    unordered_map<string,vector<pp>>mp;
+    unordered_map<string,vector<pair<int,string>>>db;
     TimeMap() {
         
     }
     
     void set(string key, string value, int timestamp) {
-        mp[key].push_back({timestamp,value});
+        db[key].push_back({timestamp,value});
         return;
     }
     
     string get(string key, int timestamp) {
-        // we have to get teh value of this time stanmp;;
-        string ans = "";
         int start = 0;
-        int end = mp[key].size()-1;
+        int end = db[key].size()-1;
+        string ans = "";
         while(start<=end){
             int mid = start+(end-start)/2;
-            if(mp[key][mid].first <= timestamp){
-                ans = mp[key][mid].second;
+            if(db[key][mid].first<=timestamp){
+                ans = db[key][mid].second;
                 start = mid+1;
             }
             else{
                 end = mid-1;
             }
         }
-
         return ans;
     }
 };
