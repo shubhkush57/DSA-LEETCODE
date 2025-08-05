638df4831cc15b9924ead72a149240301783fb1d
Shubham Kushwaha <76884219+shubhkush57@users.noreply.github.com>
2025-08-05 18:49:03 +0530

    Time: 53 ms (8.21%), Space: 97.6 MB (7.27%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.cpp
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.cpp
@@ -12,62 +12,46 @@ public:
 
     // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
-        if(root == NULL){
-            return "()";
-        }
-        string LeftPart = serialize(root->left);
-        string RightPart = serialize(root->right);
-        int leftSize = LeftPart.size();
-        int rightSize = RightPart.size();
-        string rootValue = to_string(root->val);
+        if(root == NULL)return "()";
+        string left = serialize(root->left);
+        string right = serialize(root->right);
         string ans = "(";
-        ans += rootValue;
+        ans += to_string(root->val);
         ans += "#";
-        ans += to_string(leftSize)+LeftPart;
+        ans += to_string(left.size())+left;
         ans += "#";
-        ans += to_string(rightSize)+RightPart;
+        ans += to_string(right.size())+right;
         ans += ")";
         return ans;
     }
 
     // Decodes your encoded data to tree.
     TreeNode* deserialize(string data) {
-        int n = data.size();
-        if(data == "()"){
-            return NULL;
-        }
-        string rootValue = "";
+        if(data == "()")return NULL;
         int i = 1;
         while(data[i] != '#'){
-            rootValue.push_back(data[i]);
             i++;
         }
-        int rootVal = stoi(rootValue);
-        TreeNode* root = new TreeNode(rootVal);
-        i++;
-        string LeftSize = "";
-        while(data[i]!= '('){
-            LeftSize.push_back(data[i]);
-            i++;
-        }
-        int leftSize = stoi(LeftSize);
-        root->left = deserialize(data.substr(i,leftSize));
-        i = leftSize+i+1;
-        string RightSize = "";
+        int rootVal = stoi(data.substr(1,i));
+        TreeNode* ans = new TreeNode(rootVal);
+        
+        int j = i+1;
         while(data[i] != '('){
-            RightSize.push_back(data[i]);
             i++;
         }
-        int rigthSize = stoi(RightSize);
-        root->right = deserialize(data.substr(i,rigthSize));
-        return root;
+        int leftPartSize = stoi(data.substr(j,i-j));
+       ans->left = deserialize(data.substr(i,leftPartSize));
+        i = i+leftPartSize;
+        j = i+1;
+        while(data[i] != '('){
+            i++;
+        }
+        int rightPartSize = stoi(data.substr(j,i-j));
+        ans->right = deserialize(data.substr(i,rightPartSize));
+        return ans;
     }
 };
 
 // Your Codec object will be instantiated and called as such:
 // Codec ser, deser;
 // TreeNode* ans = deser.deserialize(ser.serialize(root));
-// (rootValue#LeftSize LeftPart#RightSize RightPart)
-
-// j-i+1 = leftSize;
-// j = leftSize+i-1;
