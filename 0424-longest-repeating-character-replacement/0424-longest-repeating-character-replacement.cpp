b429ba60beb1c7a3b99e966de01b000ccf3ab3f0
Shubham Kushwaha <76884219+shubhkush57@users.noreply.github.com>
2025-09-21 22:48:51 +0530

    Time: 22 ms (9.44%), Space: 10.8 MB (69.98%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        // it continng the same letters....
-        // so we have to start from the 
         int n = s.size();
         int ans = 0;
-        // now we have to take the size.
         for(char ch = 'A';ch<='Z';ch++){
-            int i = 0,j = 0;
-            int repl = k;
+            // we are searching for the s
+            int i = 0,j = 0,k_= k;
             while(j<n){
-                if(s[j] != ch){
-                    k--;
-                }
-                while(i<j && k<0){
-                    // now we hvae to remove the charactess...
+               if(s[j] != ch){
+                 k_--;
+               }
+               while(i<j && k_<0){
                     if(s[i] != ch){
-                        k++;
+                        k_++;
                     }
                     i++;
-                }
+               }
                 ans = max(ans,j-i+1);
-                j++;
+               j++;
+
             }
-            k = repl;
         }
 
         return ans;
