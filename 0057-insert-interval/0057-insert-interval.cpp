ad47f84408b1d4f1d214df2533ef483b0739289e
Shubham Kushwaha <76884219+shubhkush57@users.noreply.github.com>
2025-09-21 23:18:33 +0530

    Time: 5 ms (11.05%), Space: 21.8 MB (20.03%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,60 +1,22 @@
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& ni) {
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+
         vector<vector<int>>ans;
+        intervals.push_back(newInterval);
+        sort(intervals.begin(),intervals.end());
         int n = intervals.size();
-        bool flag = false;
-        for(int i = 0;i<n;i++){
-            if(flag == false){
-                if(ni[1]<intervals[i][0]){
-                    ans.push_back(ni);
-                    ans.push_back(intervals[i]);
-                    flag = true;
-                }
-                else if(ni[1] == intervals[i][0]){
-                    intervals[i][0]= min(ni[0],intervals[i][0]);
-                    intervals[i][1] = max(ni[1],intervals[i][1]);
-                    ans.push_back(intervals[i]);
-                    flag = true;
-                }
-                else if(ni[1] >=intervals[i][0] && ni[1] <=intervals[i][1]){
-                    intervals[i][0] = min(ni[0],intervals[i][0]);
-                    intervals[i][1] = max(ni[1],intervals[i][1]);
-                    ans.push_back(intervals[i]);
-                    flag = true;
-                }
-                else if(ni[0]<= intervals[i][0] && ni[1] >= intervals[i][1]){
-                    ans.push_back(ni);
-                    flag = true;
-                }
-                // mainn conditions
-                else if(ni[0]>=intervals[i][0] && ni[0] <=intervals[i][1]){
-                    intervals[i][0] = min(intervals[i][0],ni[0]);
-                    intervals[i][1]= max(intervals[i][1],ni[1]);
-                    ans.push_back(intervals[i]);
-                    flag = true;
-                }
-                else{
-                    ans.push_back(intervals[i]);
-                }
+        ans.push_back(intervals[0]);
+        for(int i = 1;i<n;i++){
+            if(intervals[i][0]<=ans.back()[1]){
+                ans.back()[1]= max(intervals[i][1],ans.back()[1]);
             }
             else{
-                // we have inserted  the ni now
-                int j = ans.size();
-                if(intervals[i][0]>=ans[j-1][0] && intervals[i][0]<=ans[j-1][1]){
-                    ans[j-1][0] = min(intervals[i][0],ans[j-1][0]);
-                    ans[j-1][1] = max(intervals[i][1],ans[j-1][1]);
-                    // ans.push_back(intervals[i])
-                }
-                else{
-                    ans.push_back(intervals[i]);
-                }
+                ans.push_back(intervals[i]);
             }
-            
         }
-        if(flag == false){
-            ans.push_back(ni);
-        }
+
+
         return ans;
     }
 };
