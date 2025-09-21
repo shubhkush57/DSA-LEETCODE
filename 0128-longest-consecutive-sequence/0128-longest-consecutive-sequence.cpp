00cdbffaaaff9c6bc0df09544b17c6d1ff3e2f12
Shubham Kushwaha <76884219+shubhkush57@users.noreply.github.com>
2025-09-21 19:15:31 +0530

    Time: 87 ms (41.59%), Space: 88.9 MB (30.65%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,34 +1,20 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        // unordered_set<int> numSet(nums.begin(), nums.end());
-        // int longest = 0;
-
-        // for (int num : numSet) {
-        //     if (numSet.find(num - 1) == numSet.end()) {
-        //         int length = 1;
-        //         while (numSet.find(num + length) != numSet.end()) {
-        //             length++;
-        //         }
-
-        //         longest = max(longest, length);
-        //     }
-        // }
-
-        // return longest;
-        int n = nums.size();
-        unordered_set<int>st(nums.begin(),nums.end());
-        int longest = 0;
-        for(auto num: st){
-            if(!st.count(num+1)){
-                int length  = 1;
-                while(st.count(num-length)){
-                    length++;
+        unordered_map<int,int>mp;
+        for(auto it: nums)mp[it]++;
+        int ans = 0;
+        for(auto it: mp){
+            int ele = it.first;
+            if(!mp.count(ele+1)){
+                int len = 1;
+                while(mp.count(ele-len)){
+                    len++;
                 }
-                longest = max(longest,length);
+                ans = max(ans,len);
             }
         }
 
-        return longest;
+        return ans;
     }
 };
