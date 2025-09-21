4352ee578bae55eb9768ef527cb8b0e59f6fb8ad
Shubham Kushwaha <76884219+shubhkush57@users.noreply.github.com>
2025-09-21 18:59:39 +0530

    Time: 0 ms (100%), Space: 42.2 MB (16.28%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        // without division operations..
         int n = nums.size();
-        vector<int>ans(n);
-        // we have to return the answer...
-        vector<int>pro_left(n,1);
-        vector<int>pro_right(n,1);
-        pro_left[0] = nums[0];
-        pro_right[n-1]= nums[n-1];
+        vector<int>left(n,1),right(n,1);
         for(int i = 1;i<n;i++){
-            pro_left[i] = pro_left[i-1]*nums[i];
+            left[i] = left[i-1]*nums[i-1];
         }
         for(int i = n-2;i>=0;i--){
-            pro_right[i] = pro_right[i+1]*nums[i];
+            right[i] = right[i+1]*nums[i+1];
         }
+        vector<int>ans(n);
         for(int i = 0;i<n;i++){
-            int pro_ex = (i == 0)?1: pro_left[i-1];
-            pro_ex *= (i == n-1)? 1: pro_right[i+1];
-            ans[i] = pro_ex;
-
+            ans[i] = left[i]*right[i];
         }
-
         return ans;
+
     }
 };
