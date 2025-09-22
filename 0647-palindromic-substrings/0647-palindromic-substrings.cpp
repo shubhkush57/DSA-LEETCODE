321e9ce82351353d51a693a9f1ebbfdd3eb1847d
Shubham Kushwaha <76884219+shubhkush57@users.noreply.github.com>
2025-09-22 21:59:12 +0530

    Time: 0 ms (100%), Space: 8.5 MB (92.57%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
-    bool solve(string &s,int i,int j,vector<vector<int>>&dp){
-        int n = s.size();
-        if(i>j || i>=n || j<0)return true;
-        if(dp[i][j] !=-1)return dp[i][j];
-        if(i+1<n)
-        dp[i+1][j] = solve(s,i+1,j,dp);
-        if(j-1>=0)
-        dp[i][j-1] = solve(s,i,j-1,dp);
-
-        return dp[i][j] = ((s[i] == s[j]) && solve(s,i+1,j-1,dp));
-    }
     int countSubstrings(string s) {
-        // let's do a queick dp 
         int n = s.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        solve(s,0,n-1,dp);
         int ans = 0;
         for(int i = 0;i<n;i++){
-            for(int j = i;j<n;j++){
-                ans += dp[i][j];
-            }
+            int even = countPail(s,i,i+1);
+            int odd = countPail(s,i,i);
+            ans += even+odd;
         }
+        return ans;
+    }
 
+    int countPail(string &s,int i,int j){
+        int ans = 0;
+        int n = s.size();
+        if(i == j){
+            ans++;i--,j++;
+        }
+        while(i>=0 && j<n){
+            if(s[i]!= s[j])break;
+            i--;j++;
+            ans++;
+        }
         return ans;
     }
 };
