cd8ea795b2c5c508d0fb60019446d3e5da21f24d
Shubham Kushwaha <76884219+shubhkush57@users.noreply.github.com>
2025-09-22 10:43:53 +0530

    Time: 0 ms (100%), Space: 24.5 MB (42.3%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -11,24 +11,18 @@
  */
 class Solution {
 public:
-    void solve(TreeNode* root,int& k,int &ans){
-        if(root == NULL){
-            return;
-        }
+    void solve(TreeNode* root, int &k,int &ans){
+        if(!root)return ;
         solve(root->left,k,ans);
         k--;
         if(k == 0){
             ans = root->val;
-            return;
         }
         solve(root->right,k,ans);
         return;
     }
     int kthSmallest(TreeNode* root, int k) {
-        if(root == NULL){
-            return -1;
-        }
-        int ans = -1;
+        int ans = 0;
         solve(root,k,ans);
         return ans;
     }
