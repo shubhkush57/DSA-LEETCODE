86e8cfa97517736fa83c3ba1f67a2015b05e9b3f
Shubham Kushwaha <76884219+shubhkush57@users.noreply.github.com>
2025-09-23 15:00:43 +0530

    Time: 227 ms (70.54%), Space: 10.4 MB (94.1%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -2,27 +2,25 @@ class Solution {
 public:
     int dx[4] = {-1,1,0,0};
     int dy[4] = {0,0,-1,1};
-    bool dfs(vector<vector<char>>& board , int i,int j, string &word,int indx){
-        int n = board.size(),m = board[0].size();
-        if(indx>= word.size())return true;
-        if(i>=n || j>= m || i<0 || j<0 || board[i][j]!= word[indx])return false;
+    bool findWord(vector<vector<char>>& board,string &word,int i,int j,int indx){
+        int m = board.size(),n = board[0].size();
+        if(indx>=word.size())return true;
+        if(i>=m || i<0 || j>=n ||j<0 || board[i][j]!= word[indx])return false;
         char temp = board[i][j];
         board[i][j] = '.';
-        for(int k  = 0;k<4;k++){
-            int nx = i+dx[k],ny =j+dy[k];
-            if(dfs(board,nx,ny,word,indx+1))return true;
-            
+        for(int k = 0;k<4;k++){
+            int nx = i+dx[k],ny = j+dy[k];
+            if(findWord(board,word,nx,ny,indx+1))return true;
         }
         board[i][j] = temp;
         return false;
-
     }
     bool exist(vector<vector<char>>& board, string word) {
-        // do backtracking..
-        int n = board.size(),m = board[0].size();
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<m;j++){
-                if(dfs(board,i,j,word,0))return true;
+        // we will be starting from each cell and backtrak
+        int m = board.size(),n = board[0].size();
+        for(int i = 0;i<m;i++){
+            for(int j =0;j<n;j++){
+                if(findWord(board,word,i,j,0))return true;
             }
         }
         return false;
