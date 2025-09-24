4ac41486d345e538f5ba9a70e91b11a7bec71c14
Shubham Kushwaha <76884219+shubhkush57@users.noreply.github.com>
2025-09-24 19:44:31 +0530

    Time: 11 ms (21.64%), Space: 20.1 MB (31.22%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,36 +1,33 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>&adjList,vector<int>&indegree,vector<int>&visited,int node){
+    bool isCompleted(vector<vector<int>>&adjList,vector<int>&indegree,vector<bool>&vis,int node){
+        if(vis[node] == true)return false;
+        vis[node] = true;
         for(auto it: adjList[node]){
-            if(visited[it] == 1)return false;
             indegree[it]--;
-            if(indegree[it] ==0){
-                visited[it] = 1;
-                if(!dfs(adjList,indegree,visited,it))return false;
+            if(indegree[it] == 0){
+                if(!isCompleted(adjList,indegree,vis,it))return false;
             }
         }
 
         return true;
     }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        int n = prerequisites.size();
         vector<vector<int>>adjList(numCourses,vector<int>());
-        vector<int>indegree(numCourses,0),visited(numCourses,0);
+        vector<int>indegree(numCourses,0);
+        vector<bool>vis(numCourses,false);
         for(auto it: prerequisites){
             adjList[it[1]].push_back(it[0]);
             indegree[it[0]]++;
         }
-        for(int i = 0;i<numCourses;i++){
-            if(indegree[i] == 0 && visited[i] == 0){
-                visited[i] = 1;
-                if(!dfs(adjList,indegree,visited,i))return false;
+        for(int i =0;i<numCourses;i++){
+            if(vis[i] == false && indegree[i] == 0){
+                if(!isCompleted(adjList,indegree,vis,i))return false;
             }
-        
         }
-        for(int i = 0;i<numCourses;i++){
-            if(visited[i] == 0)return false;
+        for(int i =0 ;i<numCourses;i++){
+            if(vis[i] == false)return false;
         }
         return true;
-
     }
 };
