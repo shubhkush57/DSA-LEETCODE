8f3bc0211af90d97346a6fc6649182433e5e7fcf
Shubham Kushwaha <76884219+shubhkush57@users.noreply.github.com>
2025-09-29 22:19:27 +0530

    Time: 14 ms (39.42%), Space: 11.4 MB (83.62%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,45 +1,31 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        // we can take the cases like..
-        int n = s.size();
-        string ans = "";
+        int m = s.size(), n = t.size();
         unordered_map<char,int>mp;
-        for(auto it: t){
-            mp[it]++;
-        }
-        int count = mp.size();
-        int first = -1,last = -1;
-        int i = 0,j = 0;
-        while(j<n){
-            if(mp.count(s[j])){
-                mp[s[j]]--;
-                if(mp[s[j]] == 0){
-                    count -=1;
+        for(auto it: t)mp[it]++;
+        int reqChar = mp.size();
+        int left = 0, right = 0, size = INT_MAX, start = 0;
+        while(right<m){
+            if(mp.count(s[right])){
+                mp[s[right]]--;
+                if(mp[s[right]]==0){
+                    reqChar--;
                 }
             }
-            while(i<=j && count <= 0){
-                if(first == -1 || (last-first+1)> (j-i+1)){
-                    first = i;
-                    last = j;
+            while(left<=right && reqChar == 0){
+                if(size>(right-left+1)){
+                    size = right-left+1;
+                    start = left;
                 }
-                if(mp.count(s[i])){
-                    mp[s[i]]++;
-                    if(mp[s[i]] == 1){
-                        count += 1;
-                    }
+                if(mp.count(s[left])){
+                    mp[s[left]]++;
+                    if(mp[s[left]] == 1)reqChar++;
                 }
-                i++;
+                left++;
             }
-
-            j++;
+            right++;
         }
-        return first == -1?"": s.substr(first,last-first+1);
-        // each answer is unique.
+        return size == INT_MAX?"": s.substr(start,size);
     }
 };
-
-// a -> 2
-
-
-
